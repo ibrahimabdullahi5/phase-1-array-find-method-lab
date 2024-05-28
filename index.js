536dc58ddc7f85e42f2e0934e7fb45590bbda514
ibrahimabdullahi5 <ibrahimabdullahibare@DESKTOP-0DEOD38.localdomain>
2024-05-28 07:16:05 -0900

    finsh arrary find methods lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+// code your solution her
+
+function superbowlWin(record) {
+    const winningRecord = record.find(game => game.result === "W");
+
+    return winningRecord ? winningRecord.year : undefined;
+  }
+  
+  const record = [
+    { year: 2014, result: "L" },
+    { year: 2015, result: "W" },
+    { year: 2016, result: "L" }
+  ];
+  
+  console.log(superbowlWin(record));
+  
